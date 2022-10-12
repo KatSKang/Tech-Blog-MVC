0b4d9d10bce3f3c77c124d8f578c2ff2a2e161c2
Kathy Kang <105574653+KatSKang@users.noreply.github.com>
2022-10-11 23:18:55 -0400

    created directory and starting files. sset up server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up sessions
+const sess = {
+  secret: 'Super secret secret',
+  cookie:  {
+    maxAge: 60 * 60 * 1000
+  },
+  resave: false,
+  httpOnly: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+// Inform Express.js which template engine we're using
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
