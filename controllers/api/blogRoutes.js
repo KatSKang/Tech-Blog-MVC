50314e6792f3ebaf0ae628688451a706dfeef620
Kathy Kang <105574653+KatSKang@users.noreply.github.com>
2022-10-21 16:45:16 -0400

    added views and updated routes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -4,11 +4,11 @@ const { Blog, Comment } = require('../../models');
 // CREATE new user
 router.post('/', async (req, res) => {
     try {
-      const newUserData = await User.create({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        password: req.body.password,
+      const newBlog = await Blog.create({
+        id: req.body.id,
+        title: req.body.title,
+        contents: req.body.contents,
+        user_id:req.findby
       });
       req.session.save(() => {
         req.session.user_id = newUserData.user_id;
