3fcd0cf6ecf73687a0283678c9ac15a75a0cc13e
Kathy Kang <105574653+KatSKang@users.noreply.github.com>
2022-11-30 01:21:10 -0500

    updt models

--- models/index.js
+++ models/index.js
@@ -3,10 +3,28 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 User.hasMany(Post, {
-  foreignKey: 'post',
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
 Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
